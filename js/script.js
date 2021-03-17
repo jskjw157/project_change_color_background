9b27ad6f3eaa34a07f18d04f3526b14ea2fd6428
jskjw157 <jskjw157@gmail.com>
2021-03-17 17:40:07 +0900

    color

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 //Choose a random color
+'use strict';
+
+const button = document.querySelector(".btn");
+const body = document.querySelector("body");
+const colors = ["red", "black", "white", "blue", "violet"];
+
+body.style.backgroundColor = "yellow";
+
+function handleClick(){
+    const colorNumber = parseInt(Math.random() * colors.length);
+    body.style.backgroundColor = colors[colorNumber];
+}
+
+function init() {
+    button.addEventListener("click", handleClick);
+}
+
+init();
